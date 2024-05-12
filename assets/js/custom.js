ecd0ea4481431ca9fd2027d1ad3397fbcf6b2656
Colton <70598503+C-Loftus@users.noreply.github.com>
2024-05-12 15:08:46 -0400

    custom js

---
+++ assets/js/custom.js
@@ -0,0 +1,16 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   var elements = document.querySelectorAll("*");
+//   elements.forEach(function (element) {
+//     if (
+//       element.childNodes.length === 1 &&
+//       element.childNodes[0].nodeType === Node.TEXT_NODE
+//     ) {
+//       if (element.textContent.includes("· Powered by Hugo & PaperMod")) {
+//         element.textContent = element.textContent.replace(
+//           "· Powered by Hugo & PaperMod",
+//           ""
+//         );
+//       }
+//     }
+//   });
+// });
